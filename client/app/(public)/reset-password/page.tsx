86da91171b2603d7f0e9b3b95e12feadff0227b4
dilip <dilip@gmail.com>
2026-02-26 16:51:54 +0530

    error solve

--- client/app/(public)/reset-password/page.tsx
+++ client/app/(public)/reset-password/page.tsx
@@ -2,9 +2,13 @@
 
 import { useResetPassMutation } from '@/app/redux/apis/auth.api'
 import { useRouter, useSearchParams } from 'next/navigation'
-import React, { useState } from 'react'
+import React, { Suspense, useState } from 'react'
 import { toast } from 'react-toastify'
 
+
+const Fallback = () => {
+    return <h1>Please wait..</h1>
+}
 const ResetPassword = () => {
     const [password, setPassword] = useState("")
     const [cpassword, setCpassword] = useState("")
@@ -30,48 +34,51 @@ const ResetPassword = () => {
     }
 
     return (
-        <div className="container d-flex justify-content-center align-items-center min-vh-100 bg-light">
-            <div className="card shadow p-4" style={{ maxWidth: "400px", width: "100%" }}>
+        <Suspense fallback={<h1>Please wait...</h1>}>
 
-                <h4 className="text-center fw-bold mb-4">Reset Password</h4>
+            <div className="container d-flex justify-content-center align-items-center min-vh-100 bg-light">
+                <div className="card shadow p-4" style={{ maxWidth: "400px", width: "100%" }}>
 
-                <form
-                    onSubmit={(e) => {
-                        e.preventDefault();
-                        handleResetPass();
-                    }}
-                >
-                    {/* New Password */}
-                    <div className="mb-3">
-                        <input
-                            type="password"
-                            placeholder="Enter new password"
-                            className="form-control"
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
-                    </div>
+                    <h4 className="text-center fw-bold mb-4">Reset Password</h4>
 
-                    {/* Confirm Password */}
-                    <div className="mb-3">
-                        <input
-                            type="password"
-                            placeholder="Confirm new password"
-                            className="form-control"
-                            onChange={(e) => setCpassword(e.target.value)}
-                        />
-                    </div>
-
-                    <button
-                        disabled={isLoading}
-                        className="btn btn-primary w-100"
-                        type="submit"
+                    <form
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            handleResetPass();
+                        }}
                     >
-                        {isLoading ? "Resetting..." : "Reset Password"}
-                    </button>
-                </form>
+                        {/* New Password */}
+                        <div className="mb-3">
+                            <input
+                                type="password"
+                                placeholder="Enter new password"
+                                className="form-control"
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
 
+                        {/* Confirm Password */}
+                        <div className="mb-3">
+                            <input
+                                type="password"
+                                placeholder="Confirm new password"
+                                className="form-control"
+                                onChange={(e) => setCpassword(e.target.value)}
+                            />
+                        </div>
+
+                        <button
+                            disabled={isLoading}
+                            className="btn btn-primary w-100"
+                            type="submit"
+                        >
+                            {isLoading ? "Resetting..." : "Reset Password"}
+                        </button>
+                    </form>
+
+                </div>
             </div>
-        </div>
+        </Suspense>
     );
 }
 
