d5c8e9d4751bab1e221b01a7886bb62c21883d35
dilip <dilip@gmail.com>
2026-02-26 16:58:57 +0530

    return error solve

--- client/app/(public)/reset-password/page.tsx
+++ client/app/(public)/reset-password/page.tsx
@@ -6,9 +6,6 @@ import React, { Suspense, useState } from 'react'
 import { toast } from 'react-toastify'
 
 
-const Fallback = () => {
-    return <h1>Please wait..</h1>
-}
 const ResetPassword = () => {
     const [password, setPassword] = useState("")
     const [cpassword, setCpassword] = useState("")
@@ -34,52 +31,59 @@ const ResetPassword = () => {
     }
 
     return (
-        <Suspense fallback={<h1>Please wait...</h1>}>
 
-            <div className="container d-flex justify-content-center align-items-center min-vh-100 bg-light">
-                <div className="card shadow p-4" style={{ maxWidth: "400px", width: "100%" }}>
 
-                    <h4 className="text-center fw-bold mb-4">Reset Password</h4>
+        <div className="container d-flex justify-content-center align-items-center min-vh-100 bg-light">
+            <div className="card shadow p-4" style={{ maxWidth: "400px", width: "100%" }}>
 
-                    <form
-                        onSubmit={(e) => {
-                            e.preventDefault();
-                            handleResetPass();
-                        }}
+                <h4 className="text-center fw-bold mb-4">Reset Password</h4>
+
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        handleResetPass();
+                    }}
+                >
+                    {/* New Password */}
+                    <div className="mb-3">
+                        <input
+                            type="password"
+                            placeholder="Enter new password"
+                            className="form-control"
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+
+                    {/* Confirm Password */}
+                    <div className="mb-3">
+                        <input
+                            type="password"
+                            placeholder="Confirm new password"
+                            className="form-control"
+                            onChange={(e) => setCpassword(e.target.value)}
+                        />
+                    </div>
+
+                    <button
+                        disabled={isLoading}
+                        className="btn btn-primary w-100"
+                        type="submit"
                     >
-                        {/* New Password */}
-                        <div className="mb-3">
-                            <input
-                                type="password"
-                                placeholder="Enter new password"
-                                className="form-control"
-                                onChange={(e) => setPassword(e.target.value)}
-                            />
-                        </div>
+                        {isLoading ? "Resetting..." : "Reset Password"}
+                    </button>
+                </form>
 
-                        {/* Confirm Password */}
-                        <div className="mb-3">
-                            <input
-                                type="password"
-                                placeholder="Confirm new password"
-                                className="form-control"
-                                onChange={(e) => setCpassword(e.target.value)}
-                            />
-                        </div>
-
-                        <button
-                            disabled={isLoading}
-                            className="btn btn-primary w-100"
-                            type="submit"
-                        >
-                            {isLoading ? "Resetting..." : "Reset Password"}
-                        </button>
-                    </form>
-
-                </div>
             </div>
-        </Suspense>
+        </div>
+
     );
 }
+const RestPass = () => {
+    return <>
+        <Suspense fallback={<h1>Please wait...</h1>}>
+            <ResetPassword />
+        </Suspense>
+    </>
+}
 
-export default ResetPassword
+export default RestPass
