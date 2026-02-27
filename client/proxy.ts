8607e8b31b8c939181302261147bf7256695fabe
dilip <dilip@gmail.com>
2026-02-27 16:52:38 +0530

    proxy checking all cookies

--- client/proxy.ts
+++ client/proxy.ts
@@ -3,7 +3,9 @@ import { NextRequest, NextResponse } from "next/server";
 export const proxy = (req: NextRequest) => {
     // step 1 check for cookie
     const token = req.cookies.get("USER")?.value
+    const all = req.cookies.getAll()
     console.log(token);
+    console.log(all);
 
     if (!token) {
         return NextResponse.redirect(new URL("/login", req.url)) //
