20d0e5d107c58c913f3a3f427111906255aa6924
dilip <dilip@gmail.com>
2026-02-27 17:00:24 +0530

    next config rewrite fix

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://bca-ly-project-jwt-server.vercel.app/api/:path*",
+      },
+    ];
+  }
 };
 
 export default nextConfig;
