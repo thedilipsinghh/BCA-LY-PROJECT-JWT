67cc671b6bb637975b3582d71c22f5de451b1b3a
dilip <dilip@gmail.com>
2026-03-02 10:35:40 +0530

    proxy and apis update fix

--- client/app/redux/apis/admin.api.ts
+++ client/app/redux/apis/admin.api.ts
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 export const adminApi = createApi({
     reducerPath: "admiApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: `${APP_URL}/api/admin`,
+        baseUrl: `/api/admin`,
         credentials: "include"
     }),
     tagTypes: ["tagName"],
